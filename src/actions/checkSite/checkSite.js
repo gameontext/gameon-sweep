4684a207da13aa24e4784c5fd909638cc3db7f63
iain <iain.duncan@uk.ibm.com>
2016-04-22 16:23:56 +0100

    Add security headers to the options
    Signed-off-by: iain <iain.duncan@uk.ibm.com>

--- src/actions/checkSite/checkSite.js
+++ src/actions/checkSite/checkSite.js
@@ -1,32 +1,44 @@
 var ws = require("nodejs-websocket");
+var crypto = require("crypto");
 const PROTOCOL = 'mediator,1.1';
 global.main = function(params) {
     console.log("Creating web socket for " + params.id + " with name " + params.name);
     
     var wsLocation = getConnectionLocation(params); 
     
-    try {
-    	var connection = ws.connect(wsLocation);
-        connection.on("connect", function() {
-            console.log("Connected");
-            connection.sendText("Hello from Node", function(anything) {
-                console.log("message sent");
-                whisk.done();
-            });
-        });
-    } catch (err) {
-    	console.log("Got error: " + err);
-    	whisk.error("Got error: " + err);
-    }
+	var connection = ws.connect(wsLocation, getWsConnectOptions(params));
+    connection.on("connect", createOnConnectHandler(connection));
     
 
     return whisk.async();
 };
 
+function createOnConnectHandler(connection) {
+	return function() {
+	    console.log("Connected");
+	    connection.sendText("Hello from Node", function(anything) {
+	        console.log("message sent");
+	        whisk.done();
+	    })
+	};
+}
+
 function getConnectionLocation(params) {
 	return params.connectionLocation;
 }
 
-function getWsConnectOptions(params) {
-	return { 'extraHeaders': {'gameon-protocol': PROTOCOL}}
+function getWsConnectionOptions(params) {
+	headers = {'gameon-protocol': PROTOCOL};
+	if (params && params.connectionSecret) {
+		var now = new Date()
+		var timestamp = now.toISOString()
+	
+		var sweepId = params.sweepId;
+		var allParams = sweepId + timestamp
+		var hash = crypto.createHmac('sha256', params.connectionSecret).update(allParams).digest('base64');
+		headers['gameon-date'] = timestamp;
+		headers['gameon-signature'] = hash;
+	}
+	options = { 'extraHeaders': headers};
+	return options;
 }
--- src/actions/checkSite/checkSiteTest.js
+++ src/actions/checkSite/checkSiteTest.js
@@ -11,12 +11,19 @@ describe('Check sites', function() {
 			assert.equal(getConnectionLocation({'connectionLocation': location}), location);
 		});
 	});
-	describe('#getHeaders', function() {
+	describe('#getWsConnectionOptions', function() {
 		it('should add the protocol header', function() {
-			assert.equal(getWsConnectOptions().extraHeaders['gameon-protocol'], 'mediator,1.1');
+			assert.equal(getWsConnectionOptions().extraHeaders['gameon-protocol'], 'mediator,1.1');
 		});
-		it.skip('should have extra headers in the options when there is a secret in the params', function() {
-			assert(getWsConnectionOptions({'connectionSecret':'a secret'}).extraHeaders);
+		it('should have signature headers in the options when there is a secret in the params', function() {
+			var headers = getWsConnectionOptions({'connectionSecret':'a secret'}).extraHeaders;
+			assert(headers);
+			assert(headers['gameon-signature']);
+			assert(headers['gameon-date']);
+			Date.parse(headers['gameon-date']);
 		});
+	});
+	describe('#createOnConnectHandler', function() {
+		
 	});
 });
