631688bc9f987d7115645cd0371a91b43e62c165
iain <iain.duncan@uk.ibm.com>
2016-05-06 11:18:53 +0100

    Tabs to spaces
    Signed-off-by: iain <iain.duncan@uk.ibm.com>

--- src/actions/loadSites/loadSites.js
+++ src/actions/loadSites/loadSites.js
@@ -1,113 +1,116 @@
 /*******************************************************************************
  * Copyright (c) 2016 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 var request = require('request');
 var crypto = require("crypto");
 var DEFAULT_SITES_URL = 'https://game-on.org/map/v1/sites';
 
 function main(params) {
 
-	request.get(buildGetSitesOptions(params), allSitesResponseHandler);
+    request.get(buildGetSitesOptions(params), allSitesResponseHandler);
 
-	return whisk.async();
+    return whisk.async();
 }
 
 function allSitesResponseHandler(error, response, body) {
-	var sites = JSON.parse(body);
-	var output = 'Have loaded ' + sites.length + ' sites.';
+    var sites = JSON.parse(body);
+    var output = 'Have loaded ' + sites.length + ' sites.';
 
-	// Record what we get back from our call to the other action and
-	// call done when they have all returned
-	var expectedResponses = sites.length;
-	var responses = new Array(sites.length);
-	var onResponse = function(i) {
-		return function(error, activation) {
-			console.log("Have returned from " + i);
-			if (error) {
-				console.log("It was an error: " + error);
-				responses[i] = {
-					"error" : error
-				};
-			} else {
-				console.log("It worked!: " + activation);
-				responses[i] = activation.result;
-			}
-			expectedResponses -= 1;
-			if (expectedResponses === 0) {
-				console.log("Nearly done, going to return: " + {
-					"elements" : responses
-				});
-				whisk.done({
-					"elements" : responses
-				});
-			}
-		}
-	}
+    // Record what we get back from our call to the other action and
+    // call done when they have all returned
+    var expectedResponses = sites.length;
+    var responses = new Array(sites.length);
+    var onResponse = function(i) {
+        return function(error, activation) {
+            console.log("Have returned from " + i);
+            if (error) {
+                console.log("It was an error: " + error);
+                responses[i] = {
+                    "error" : error
+                };
+            } else {
+                console.log("It worked!: " + activation);
+                responses[i] = activation.result;
+            }
+            expectedResponses -= 1;
+            if (expectedResponses === 0) {
+                console.log("Nearly done, going to return: " + {
+                    "elements" : responses
+                });
+                whisk.done({
+                    "elements" : responses
+                });
+            }
+        }
+    }
 
-	sites.forEach(function(site, index) {
-		var siteInformation = {
-			id : site._id
-		}
-		if (site.info != null) {
-			siteInformation.name = site.info.name;
-			if (site.info.connectionDetails != null) {
-				siteInformation.connectionLocation = site.info.connectionDetails.target;
-				siteInformation.connectionType = site.info.connectionDetails.type;
-				siteInformation.connectionSecret = site.info.connectionDetails.token;
-			}
-		}
-		whisk.invoke({
-			name : 'checkSite',
-			parameters : siteInformation,
-			blocking : true,
-			next : onResponse(index)
-		});
-	});
+    sites
+            .forEach(function(site, index) {
+                var siteInformation = {
+                    id : site._id
+                }
+                if (site.info != null) {
+                    siteInformation.name = site.info.name;
+                    if (site.info.connectionDetails != null) {
+                        siteInformation.connectionLocation = site.info.connectionDetails.target;
+                        siteInformation.connectionType = site.info.connectionDetails.type;
+                        siteInformation.connectionSecret = site.info.connectionDetails.token;
+                    }
+                }
+                whisk.invoke({
+                    name : 'checkSite',
+                    parameters : siteInformation,
+                    blocking : true,
+                    next : onResponse(index)
+                });
+            });
 }
 
 function buildGetSitesOptions(params) {
-	var url;
-	if (params && params.mapSitesUrl) {
-		url = params.mapSitesUrl;
-	} else {
-		url = DEFAULT_SITES_URL;
-	}
-	var options = { url : url };
-	if (params && params.sweepId && params.sweepApiKey) {
-		addSecurityHeaders(options, params);
-	}
-	return options;
+    var url;
+    if (params && params.mapSitesUrl) {
+        url = params.mapSitesUrl;
+    } else {
+        url = DEFAULT_SITES_URL;
+    }
+    var options = {
+        url : url
+    };
+    if (params && params.sweepId && params.sweepApiKey) {
+        addSecurityHeaders(options, params);
+    }
+    return options;
 }
 
 function addSecurityHeaders(options, params) {
-	var now = new Date()
-	var timestamp = now.toISOString()
+    var now = new Date()
+    var timestamp = now.toISOString()
 
-	console.log("Now!: " + now)
-	console.log("Timestamp: " + timestamp)
+    console.log("Now!: " + now)
+    console.log("Timestamp: " + timestamp)
 
-	var sweepId = params.sweepId;
-	var allParams = sweepId + timestamp
-	var hash = crypto.createHmac('sha256', params.sweepApiKey).update(allParams)
-			.digest('base64')
+    var sweepId = params.sweepId;
+    var allParams = sweepId + timestamp
+    var hash = crypto.createHmac('sha256', params.sweepApiKey)
+            .update(allParams).digest('base64')
 
-	console.log("HASH : " + hash)
-	options.headers = {
-		'Content-Type' : 'application/json',
-		'gameon-id' : sweepId,
-		'gameon-date' : timestamp,
-		'gameon-signature' : hash
-	}
+    console.log("HASH : " + hash)
+    options.headers = {
+        'Content-Type' : 'application/json',
+        'gameon-id' : sweepId,
+        'gameon-date' : timestamp,
+        'gameon-signature' : hash
+    }
 }
